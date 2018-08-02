f1412603ec4f8873bb5af76f40daf045c7e08464
teemal <taymal1987@gmail.com>
2018-08-02 13:28:32 -0700

    fix tab switching

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
 	var lang_click = 1;
+
 	$(".left_arrow").click(function(){
-        $("p").hide();
+        $("#logo_" + lang_click).hide();
+        lang_click--;
+        if (lang_click <= 0) {
+        	lang_click = 9;
+        };
+        $("#logo_" + lang_click).show();
     });
 
 	$(".right_arrow").click(function(){
-        $("#logo" + lang_click).hide();
+        $("#logo_" + lang_click).hide();
         lang_click++;
-        $("#logo" + lang_click).show();
+        if (lang_click > 9) {
+        	lang_click = 1;
+        };
+        $("#logo_" + lang_click).show();
+    });
+
+    $("#head-home").click(function(){
+    	$(".lang").hide();
+    	$(".exp").hide();
+    	$(".home").css("display", "grid");
+    });
+
+    $("#head-lang").click(function(){
+    	$(".home").hide();
+    	$(".exp").hide();
+    	$(".lang").css("display", "grid");
     });
 });
