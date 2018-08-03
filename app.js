85d212af5c4a52a712978d2fd8c86c684dcb20aa
teemal <taymal1987@gmail.com>
2018-08-03 12:46:51 -0700

    center images and align directional arrows

--- app.js
+++ app.js
@@ -3,20 +3,24 @@ $(document).ready(function(){
 
 	$(".left_arrow").click(function(){
         $("#logo_" + lang_click).hide();
+        $("#blurb_" + lang_click).hide();
         lang_click--;
         if (lang_click <= 0) {
         	lang_click = 9;
         };
         $("#logo_" + lang_click).show();
+        $("#blurb_" + lang_click).show();
     });
 
 	$(".right_arrow").click(function(){
         $("#logo_" + lang_click).hide();
+        $("#blurb_" + lang_click).hide();
         lang_click++;
         if (lang_click > 9) {
         	lang_click = 1;
         };
         $("#logo_" + lang_click).show();
+        $("#blurb_" + lang_click).show();
     });
 
     $("#head-home").click(function(){
